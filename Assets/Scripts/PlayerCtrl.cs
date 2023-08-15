5685257a5a0af707989fb5ce0d82e9d4996aade2
Boldoboldin <henriquebaldocarlos@gmail.com>
2023-08-14 21:50:34 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,50 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    [SerializeField] private float speed;
+
+    [Header("Shot Variables")]
+    [SerializeField] private GameObject playerShot;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float shotTimer;
+    private float shotCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Pegando os inputs
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 playerVel = new Vector2(horizontal, vertical);
+
+        playerVel.Normalize();
+
+        rb2D.velocity = playerVel * speed;
+
+        if (Input.GetButton("Fire1") && shotCount <= 0)
+        {
+            fire();
+            shotCount -= Time.deltaTime;
+        }
+    }
+
+    void fire()
+    {        
+        Instantiate(playerShot, firePoint.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/ShotCtrl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotCtrl : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    [SerializeField] private float shotSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.velocity = new Vector2(0f, shotSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
